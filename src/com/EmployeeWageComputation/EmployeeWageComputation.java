53daa1aef979a442f2fdd7f5a5a446a7c9e51aa9
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-07 15:27:04 +0530

    Added :Welcome Massege

---
+++ src/com/EmployeeWageComputation/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.EmployeeWageComputation;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to employee Wage Computation");
+	}
+}
