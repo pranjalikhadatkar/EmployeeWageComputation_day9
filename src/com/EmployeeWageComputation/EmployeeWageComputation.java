7a9e135d60204d6e0a29bc226325f33cce2b1f55
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-07 16:56:39 +0530

    added : Employee wages for multiple company

--- src/com/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/com/EmployeeWageComputation/EmployeeWageComputation.java
@@ -3,45 +3,38 @@ package com.EmployeeWageComputation;
 public class EmployeeWageComputation {
 	public static final  int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_WAGES_PER_HOUR = 20;
-	public static final int NO_OF_WORKING_DAYS = 20;
-	public static final int MAX_HOUR_IN_MONTH = 100;
 	
-	public void computwages(){
-		int empHrs = 0;
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
-		while (totalEmpHrs <= MAX_HOUR_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS ) {
-			totalWorkingDays++;
-			
-		int empcheck = (int)Math.round(Math.random() * 10) %3;
-		  switch (empcheck)
-		{
-		 case IS_PART_TIME:
-			 		empHrs = 4;
-			 		break;
-		 case IS_FULL_TIME:
-			 		empHrs = 8;
-			 		break;
-		 default:
-			 	empHrs = 0;
-			 	break;
-		}
-		  
-		totalEmpHrs += empHrs;
-		System.out.println("Day#: " + totalWorkingDays + "empHrs: " + empHrs);
-	}
-		int totalempwages = totalEmpHrs * EMP_WAGES_PER_HOUR;
+	public static int computeEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
+	{
+		int empHrs = 0,totalEmpHrs = 0,totalWorkingDays = 0;
 		
-	System.out.println("totalEmpWages :" + totalempwages);	
-	return ;
-	}
+		while(totalEmpHrs<=maxHoursPerMonth && totalWorkingDays<numOfWorkingDays)
+		{
+			totalWorkingDays++;
+			int empcheck= (int) Math.floor(Math.random() * 10) %3;
+			switch (empcheck)
+			{
+			 case IS_PART_TIME:
+				 		empHrs = 4;
+				 		break;
+			 case IS_FULL_TIME:
+				 		empHrs = 8;
+				 		break;
+			 default:
+				 	empHrs = 0;
+				 	break;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day: " + totalWorkingDays + "empHrs: " + empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("totalEmpWagesforcompany is: " + totalEmpWage);
+		return totalEmpWage;	
+		}
 	
 	public static void main(String[] args) {
-		EmployeeWageComputation obj=new EmployeeWageComputation();
-		obj.computwages();	
-		      
-				
-		    }
+		computeEmpWage("company1 ",20,2,10);
+		computeEmpWage("company2 ",10,4,20);
+	}
 
 }
