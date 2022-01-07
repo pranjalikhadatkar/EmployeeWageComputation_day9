ca1a1215c4bd3e748e5ef1c86711ae605ea330aa
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-07 16:30:48 +0530

    Added : Employee maximum wages per month

--- src/com/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/com/EmployeeWageComputation/EmployeeWageComputation.java
@@ -4,14 +4,15 @@ public class EmployeeWageComputation {
 	public static final  int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_WAGES_PER_HOUR = 20;
-	public static final int NO_OF_WORKING_DAYS = 2;
+	public static final int NO_OF_WORKING_DAYS = 20;
+	public static final int MAX_HOUR_IN_MONTH = 100;
 	
-	public void Empwagesformonth() {
-		int totalempwages = 0;
+	public void Empmaxwagesinmonth(){
 		int empHrs = 0;
-		int empWages = 0;
-		
-		for (int day = 0; day < NO_OF_WORKING_DAYS; day++ ) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HOUR_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS ) {
+			totalWorkingDays++;
 			
 		int empcheck = (int)Math.round(Math.random() * 10) %3;
 		  switch (empcheck)
@@ -26,17 +27,17 @@ public class EmployeeWageComputation {
 			 	empHrs = 0;
 			 	break;
 		}
-		empWages = empHrs * EMP_WAGES_PER_HOUR;
-		totalempwages += empWages;
-		System.out.println("empWages :" + empWages);
+		  
+		totalEmpHrs += empHrs;
+		System.out.println("Day#: " + totalWorkingDays + "empHrs: " + empHrs);
 	}
-	System.out.println("totalEmpWages :" + totalempwages);
+		int totalempwages = totalEmpHrs * EMP_WAGES_PER_HOUR;
 		
+	System.out.println("totalEmpWages :" + totalempwages);	
 	}
 	
 	public static void main(String[] args) {
 		EmployeeWageComputation obj=new EmployeeWageComputation();
-		obj.Empwagesformonth();		
-		
-    }
+		obj.Empmaxwagesinmonth();			
+	}
 }
