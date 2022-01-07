138cdb62f407175c9dbe5b6b6460c40fc289f518
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-07 15:57:12 +0530

    Added : employee full time present or absent

--- src/com/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/com/EmployeeWageComputation/EmployeeWageComputation.java
@@ -1,24 +1,25 @@
 package com.EmployeeWageComputation;
 
 public class EmployeeWageComputation {
-	final int FullTimeEmployee = 1;
-	  
-	public void checkempstatus() {
-		int empstatus = (int) (Math.random() * 10) % 2;
-        if (empstatus == FullTimeEmployee)
-        {
-            System.out.println("Employee is Present");
-        }
-        else
-        {
-            System.out.println("Employee is Absent");
-        }
-	}
-
-	public static void main(String[] args) {
+	final int IS_FULL_TIME = 1;
+	final int EMP_WAGES_PER_HOUR = 20;
+	
+	public void Empwagesperhr(){
+		int empHrs = 0;
+		int empWages = 0;
 		
+			double empCheck = Math.floor(Math.random() * 10 ) % 2;
+	         if (empCheck == IS_FULL_TIME)
+	        	 empHrs = 8;
+	         else
+	        	 empHrs = 0;
+	         empWages = empHrs * EMP_WAGES_PER_HOUR;
+	         System.out.println("empWages: " + empWages);
+	}
+	
+	public static void main(String[] args) {
 		EmployeeWageComputation obj=new EmployeeWageComputation();
-		obj.checkempstatus();
+		obj.Empwagesperhr();			
 	}
 }
 
