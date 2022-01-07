f5a917144f42396a6638e33f96517b428898b9a1
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-07 16:14:02 +0530

    Added : employee wages per hour

--- src/com/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/com/EmployeeWageComputation/EmployeeWageComputation.java
@@ -1,33 +1,32 @@
 package com.EmployeeWageComputation;
 
 public class EmployeeWageComputation {
-	 final int IS_PART_TIME = 1;
-	  final int IS_FULL_TIME = 2;
-	  final int EMP_WAGES_PER_HOUR = 20;
-	  
-	  public void Empwagesperhr() {
-		  int emphrs = 0;
-		    int empwages = 0;
-
-		    int empcheck =(int)Math.round((Math.random()));
-		    System.out.println(empcheck);
-		    if (empcheck == IS_PART_TIME)
-		    {
-		    	emphrs = 4;
-		    }
-		    else if (empcheck == IS_FULL_TIME)
-		    {
-		    	emphrs = 8;
-		    }
-		    else
-		    {
-		    	emphrs = 0;
-		    }
-		    empwages = emphrs * EMP_WAGES_PER_HOUR;
-		    System.out.println("emp wages:" + empwages); 
-	  }
+	final int IS_PART_TIME = 1;
+	 final int IS_FULL_TIME = 2;
+	 final int EMP_WAGES_PER_HOUR = 20;
+	
+	public void Empwageperhour(){
+		int empHrs = 0;
+		int empWages = 0;
+		
+		int empcheck = (int)Math.round(Math.random());
+		switch (empcheck)
+		{
+		 case IS_PART_TIME:
+			 		empHrs = 4;
+			 		break;
+		 case IS_FULL_TIME:
+			 		empHrs = 8;
+			 		break;
+		 default:
+			 	empHrs = 0;
+			 	break;
+		}
+		empWages = empHrs * EMP_WAGES_PER_HOUR;
+		System.out.println("emp wages:" + empWages);
+	}
 	public static void main(String[] args) {
 		EmployeeWageComputation obj=new EmployeeWageComputation();
-		obj.Empwagesperhr();		
-   }
+		obj.Empwageperhour();	
+		}	
 }
