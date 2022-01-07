27569088bc6e4db9011ef3bb209d1ae33a171fdd
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-07 15:42:07 +0530

    Added : employee present or absent

--- src/com/EmployeeWageComputation/EmployeeWageComputation.java
+++ src/com/EmployeeWageComputation/EmployeeWageComputation.java
@@ -1,8 +1,24 @@
 package com.EmployeeWageComputation;
 
 public class EmployeeWageComputation {
+	final int FullTimeEmployee = 1;
+	  
+	public void checkempstatus() {
+		int empstatus = (int) (Math.random() * 10) % 2;
+        if (empstatus == FullTimeEmployee)
+        {
+            System.out.println("Employee is Present");
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
+        }
+	}
 
 	public static void main(String[] args) {
-	System.out.println("Welcome to employee Wage Computation");
+		
+		EmployeeWageComputation obj=new EmployeeWageComputation();
+		obj.checkempstatus();
 	}
 }
+
